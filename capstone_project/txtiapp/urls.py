ac2237f97dc6eb739a291ff80825cff09a36931e
nocnoc_e <dhgustmd25@hotmail.com>
2023-03-14 21:53:52 -0400

    added stability_ai, update views and urls

--- capstone_project/txtiapp/urls.py
+++ capstone_project/txtiapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # your other URL routes here
+    path('generate-image/', views.generate_image_view, name='generate_image'),
 ]
--- capstone_project/txtiapp/views.py
+++ capstone_project/txtiapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .stability_ai import generate_image
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'app_name/home.html')
+
+def generate_image_view(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        image_data = generate_image(text)
+        if image_data:
+            return HttpResponse(image_data, content_type='image/png')
+    return render(request, 'app_name/generate_image.html')
+
 
 
 # Create your views here.
